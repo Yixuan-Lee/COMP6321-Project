7847e6202c95d05bf6883a75fa451583435063db
liyixuan <liyixuan4030614@gmail.com>
2019-10-27 12:39:14 -0400

    apply GridSearchCV or RandomSearchCV to select the best model

--- models/classification/1_Diabetic_Retinopathy.py
+++ models/classification/1_Diabetic_Retinopathy.py
@@ -1,16 +1,29 @@
 import os
 import numpy as np
-from models import settings
-from scipy.io import arff
-from k_nearest_neighbours import K_nearest_neighbours
-from support_vector_classifier import Support_vector_classifier
-from decision_tree_classifier import Decision_tree_classifier
-from random_forest_classifier import Random_forest_classifier
-from ada_boost_classifier import Ada_boost_classifier
-from logistic_regression import Logistic_regression
+import scipy
+import scipy.stats              # For reciprocal distribution
+import sklearn.model_selection  # for RandomizedSearchCV
+from models import settings     # for retrieving root path
+from scipy.io import arff       # for loading .arff file
+from sklearn.neighbors import KNeighborsClassifier
+# from k_nearest_neighbours import K_nearest_neighbours
+from sklearn.svm import SVC
+# from support_vector_classifier import Support_vector_classifier
+from sklearn.tree import DecisionTreeClassifier
+# from decision_tree_classifier import Decision_tree_classifier
+from sklearn.ensemble import RandomForestClassifier
+# from random_forest_classifier import Random_forest_classifier
+from sklearn.ensemble import AdaBoostClassifier
+# from ada_boost_classifier import Ada_boost_classifier
+from sklearn.linear_model import LogisticRegression
+# from logistic_regression import Logistic_regression
+
 from gaussian_naive_bayes import Gaussian_naive_bayes
+from sklearn.neural_network import MLPClassifier
 from neural_network_classifier import Neural_network_classifier
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 class Diabetic_retinopathy:
@@ -28,63 +41,316 @@ class Diabetic_retinopathy:
         # read the data file
         file, meta = arff.loadarff(os.path.join(settings.ROOT_DIR, filepath,
             filename))
-        self.data = np.asarray(file.tolist(), dtype=np.float32)
-        self.data = self.data[:, :-1]
-        self.targets = self.data[:, -1]
+        file_data = np.asarray(file.tolist(), dtype=np.float32)
+        self.data = file_data[:, :-1]   # (1151, 19)
+        self.targets = file_data[:, -1]  # (1151, )
 
         # split into the train and test sets
         self.x_train, self.x_test, self.y_train, self.y_test = \
             train_test_split(self.data, self.targets, test_size=0.33,
                 random_state=0)
 
+        # normalize the training set
+        scaler = preprocessing.StandardScaler().fit(self.x_train)
+        self.x_train = scaler.transform(self.x_train)
+        # normalize the test set with the train-set mean and std
+        self.x_test = scaler.transform(self.x_test)
+
+    ##################### Model training #####################
     def k_nearest_neighbours(self):
-        knn = K_nearest_neighbours()
-        knn.train(self.x_train, self.y_train)
-        return knn.get_accuracy(self.x_test, self.y_test)
+        """
+        for knn, i train on the training data using different :
+            1) n_neighbors,
+            2) weights
+            3) p
+        since these 3 parameters vary in small range, so i choose to
+        use GridSearchCV
+
+        :return: test accuracy of the knn best model
+        """
+
+        # Grid Search Validation
+        # define param_grid argument to give GridSearchCV
+        n_neighbors = range(1, 100, 2)  # [1, 3, 5, ..., 99]
+        weights = ['uniform', 'distance']
+        p = [1, 2]
+        param_grid = {
+            'n_neighbors': n_neighbors,
+            'weights': weights,
+            'p': p
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=KNeighborsClassifier(),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print('candidates: ', param_grid)
+        print(gscv.best_params_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        test_accuracy = accuracy_score(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return test_accuracy
 
     def support_vector_classifier(self):
-        svc = Support_vector_classifier()
-        svc.train(self.x_train, self.y_train)
-        return svc.get_accuracy(self.x_test, self.y_test)
+        """
+        for svc, i train on the training data using different :
+            2) C
+            3) gamma
+        :return: test accuracy of the svc best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        C = np.logspace(start=-1, stop=3, base=10, num=5, dtype=np.float32)  # [0.1, 1, 10, 100, 1000]
+        gamma = np.logspace(start=-1, stop=1, base=10, num=3, dtype=np.float32)  # [0.01, 0.1, 1, 10]
+        param_grid = {
+            'C': C,
+            'gamma': gamma,
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=SVC(kernel='rbf', random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print('candidates: ', param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        test_accuracy = accuracy_score(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return test_accuracy
 
     def decision_tree_classifier(self):
-        dtc = Decision_tree_classifier()
-        dtc.train(self.x_train, self.y_train)
-        return dtc.get_accuracy(self.x_test, self.y_test)
+        """
+        for dtc, i train on the training data using different :
+            1) criterion
+            2) max_depth
+
+        :return: test accuracy of the dtc best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        criterion = ['gini', 'entropy']
+        max_depth = range(1, 100, 2)
+        param_grid = {
+            'criterion': criterion,
+            'max_depth': max_depth
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=DecisionTreeClassifier(random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print('candidates: ', param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        test_accuracy = accuracy_score(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return test_accuracy
 
     def random_forest_classifier(self):
-        rfc = Random_forest_classifier()
-        rfc.train(self.x_train, self.y_train)
-        return rfc.get_accuracy(self.x_test, self.y_test)
+        """
+        for rfc, i train on the training data using different :
+            1) criterion
+            2) max_depth
+
+        :return: test accuracy of the dtc best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        criterion = ['gini', 'entropy']
+        max_depth = range(1, 20, 2)
+        param_grid = {
+            'criterion': criterion,
+            'max_depth': max_depth
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=RandomForestClassifier(n_estimators=100, random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print('candidates: ', param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        test_accuracy = accuracy_score(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return test_accuracy
+
 
     def ada_boost_classifier(self):
-        abc = Ada_boost_classifier()
-        abc.train(self.x_train, self.y_train)
-        return abc.get_accuracy(self.x_test, self.y_test)
+        """
+        for abc, i train on the training data using different :
+            1) n_estimators
+            2) learning_rate
+
+        :return: test accuracy of the dtc best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        n_estimators = range(1, 100, 5)
+        learning_rate = np.logspace(start=-2, stop=0, base=10, num=3,
+            dtype=np.float32)
+        param_grid = {
+            'n_estimators': n_estimators,
+            'learning_rate': learning_rate
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=AdaBoostClassifier(algorithm='SAMME', random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print('candidates: ', param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        test_accuracy = accuracy_score(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return test_accuracy
 
     def logistic_regression(self):
-        lr = Logistic_regression()
-        lr.train(self.x_train, self.y_train)
-        return lr.get_accuracy(self.x_test, self.y_test)
+        """
+        for lr, i train on the training data using different :
+            1) C
+
+        :return: test accuracy of the dtc best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        C = np.logspace(start=-4, stop=4, base=10, num=9, dtype=np.float32)
+        param_grid = {
+            'C': C
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=LogisticRegression(max_iter=10000, solver='lbfgs',
+                random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print('candidates: ', param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        test_accuracy = accuracy_score(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return test_accuracy
 
     def gaussian_naive_bayes(self):
+        """
+        for gnb, i train on the training data using different :
+            1)
+            2)
+
+        :return: test accuracy of the gnb best model
+        """
         gnb = Gaussian_naive_bayes()
         gnb.train(self.x_train, self.y_train)
         return gnb.get_accuracy(self.x_test, self.y_test)
 
     def neural_network_classifier(self):
-        nnc = Neural_network_classifier(hls=(15,), s='lbfgs', alp=1e-5)
-        nnc.train(self.x_train, self.y_train)
-        return nnc.get_accuracy(self.x_test, self.y_test)
+        """
+        for nnc, i train on the training data using different :
+            1) hidden_layer_sizes
+            2) max_iter
+
+        :return: test accuracy of the nnr best model
+        """
+        # RandomSearch Cross Validation
+        # define param_dist argument to give RandomSearchCV
+        reciprocal_distrobution_hls = scipy.stats.reciprocal(a=100, b=1000)
+        reciprocal_distribution_mi = scipy.stats.reciprocal(a=1000, b=10000)
+        np.random.seed(0)
+        reci_hidden_layer_sizes = \
+            reciprocal_distrobution_hls.rvs(size=5).astype(np.int)
+        max_iter = reciprocal_distribution_mi.rvs(size=5).astype(np.int)
+        param_dist = {
+            'hidden_layer_sizes': reci_hidden_layer_sizes,
+            'max_iter': max_iter
+        }
+
+        # fit a 5-fold RandomSearchCV instance
+        rscv = sklearn.model_selection.RandomizedSearchCV(
+            estimator=MLPClassifier(activation='relu', solver='lbfgs',
+                alpha=1e-5, random_state=0),
+            param_distributions=param_dist,
+            verbose=1,
+            cv=5,
+            n_jobs=5,  # 5 concurrent workers
+            n_iter=5,
+            random_state=0)
+        rscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print('candidates: ', param_dist)
+        print(rscv.best_estimator_)
+
+        # estimate on the test set
+        best_rscv = rscv.best_estimator_
+        test_accuracy = accuracy_score(
+            y_true=self.y_test,
+            y_pred=best_rscv.predict(self.x_test))
+
+        return test_accuracy
 
 
 if __name__ == '__main__':
     dr = Diabetic_retinopathy()
-    print('KNN: %.2f' % dr.k_nearest_neighbours())
-    print('SVC: %.2f' % dr.support_vector_classifier())
-    print('DTC: %.2f' % dr.decision_tree_classifier())
-    print('RFC: %.2f' % dr.random_forest_classifier())
-    print('ABC: %.2f' % dr.ada_boost_classifier())
-    print(' LR: %.2f' % dr.logistic_regression())
-    print('GNB: %.2f' % dr.gaussian_naive_bayes())
-    print('NNC: %.2f' % dr.neural_network_classifier())
+    print("accuracy on the actual test set:")
+    # print('KNN: %.2f %%' % (dr.k_nearest_neighbours() * 100))
+    # print('SVC: %.2f %%' % (dr.support_vector_classifier() * 100))
+    # print('DTC: %.2f %%' % (dr.decision_tree_classifier() * 100))
+    # print('RFC: %.2f %%' % (dr.random_forest_classifier() * 100))
+    # print('ABC: %.2f %%' % (dr.ada_boost_classifier() * 100))
+    # print(' LR: %.2f %%' % (dr.logistic_regression() * 100))
+
+    # print('GNB: %.2f %%' % (dr.gaussian_naive_bayes() * 100))
+
+    print('NNC: %.2f %%' % (dr.neural_network_classifier() * 100))
--- models/regression/1_Wine_Quality.py
+++ models/regression/1_Wine_Quality.py
@@ -1,15 +1,27 @@
 import os
 import numpy as np
-from models import settings
+import scipy
+import scipy.stats              # For reciprocal distribution
+import sklearn.model_selection  # for RandomizedSearchCV
+from models import settings     # for retrieving root path
+from sklearn.svm import SVR
 from support_vector_regressor import Support_vector_regressor
+from sklearn.tree import DecisionTreeRegressor
 from decision_tree_regressor import Decision_tree_regressor
+from sklearn.ensemble import RandomForestRegressor
 from random_forest_regressor import Random_forest_regressor
+from sklearn.ensemble import AdaBoostRegressor
 from ada_boost_regressor import Ada_boost_regressor
+
 from gaussian_process_regressor import Gaussian_process_regressor
-from linear_regression import Linear_regression
-from neural_network_regressor import Neural_network_regressor
-from sklearn.model_selection import train_test_split
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
+from sklearn.linear_model import LinearRegression
+from linear_regression import Linear_regression
+from sklearn.neural_network import MLPRegressor
+from neural_network_regressor import Neural_network_regressor
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 
 class Wine_quality:
@@ -30,58 +42,262 @@ class Wine_quality:
             delimiter=';', dtype=np.float32, skiprows=1)
         f2 = np.loadtxt(os.path.join(settings.ROOT_DIR, filepath, filename2),
             delimiter=';', dtype=np.float32, skiprows=1)
-        f = np.vstack((f1, f2))
-        self.data = f[:, :-1]
-        self.targets = f[:, -1]
+        file_data = np.vstack((f1, f2))
+        self.data = file_data[:, :-1]  # (6497, 11)
+        self.targets = file_data[:, -1]  # (6497, )
 
         # split into train and test sets
         self.x_train, self.x_test, self.y_train, self.y_test = \
             train_test_split(self.data, self.targets, test_size=0.33,
                 random_state=0)
 
+        # normalize the training set
+        self.scaler = preprocessing.StandardScaler().fit(self.x_train)
+        self.x_train = self.scaler.transform(self.x_train)
+        # normalize the test set with the train-set mean and std
+        self.x_test = self.scaler.transform(self.x_test)
+
+    ##################### Model training #####################
     def support_vector_regression(self):
-        svr = Support_vector_regressor()
-        svr.train(self.x_train, self.y_train)
-        return svr.get_mse(self.x_test, self.y_test)
+        """
+        for svr, i train on the training data using different :
+            1) C
+            2) gamma
+
+        :return: test accuracy of the svr best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        C = np.logspace(start=-1, stop=3, base=10, num=5,
+            dtype=np.float32)  # [0.1, 1, 10, 100, 1000]
+        gamma = np.logspace(start=-1, stop=1, base=10, num=3,
+            dtype=np.float32)  # [0.01, 0.1, 1, 10]
+        param_grid = {
+            'C': C,
+            'gamma': gamma,
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=SVR(kernel='rbf'),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print(param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        squared_error = mean_squared_error(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return squared_error
 
     def decision_tree_regression(self):
-        dtf = Decision_tree_regressor()
-        dtf.train(self.x_train, self.y_train)
-        return dtf.get_mse(self.x_test, self.y_test)
+        """
+        for dtr, i train on the training data using different :
+            1) criterion
+            2) max_depth
+
+        :return: test accuracy of the dtr best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        criterion = ['mse', 'mae']
+        max_depth = range(1, 20, 2)
+        param_grid = {
+            'criterion': criterion,
+            'max_depth': max_depth
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=DecisionTreeRegressor(random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print(param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        squared_error = mean_squared_error(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return squared_error
 
     def random_forest_regression(self):
-        rfr = Random_forest_regressor()
-        rfr.train(self.x_train, self.y_train)
-        return rfr.get_mse(self.x_test, self.y_test)
+        """
+        for rfr, i train on the training data using different :
+            1) n_estimators
+            2) max_depth
+
+        :return: test accuracy of the rfr best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        n_estimators = range(1, 200, 50)
+        max_depth = range(1, 20, 2)
+        param_grid = {
+            'n_estimators': n_estimators,
+            'max_depth': max_depth
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=RandomForestRegressor(random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print(param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        squared_error = mean_squared_error(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return squared_error
+
+
 
     def ada_boost_regression(self):
-        abr = Ada_boost_regressor()
-        abr.train(self.x_train, self.y_train)
-        return abr.get_mse(self.x_test, self.y_test)
+        """
+        for abr, i train on the training data using different :
+            1) n_estimators
+            2) learning_rate
+
+        :return: test accuracy of the abr best model
+        """
+
+        # GridSearch Cross Validation
+        # define param_grid argument to give GridSearchCV
+        n_estimators = range(1, 100, 5)
+        learning_rate = np.logspace(start=-2, stop=0, base=10, num=3,
+            dtype=np.float32)   # [0.01, 0.1, 1]
+        param_grid = {
+            'n_estimators': n_estimators,
+            'learning_rate': learning_rate
+        }
+
+        # fit a 5-fold GridSearchCV instance
+        gscv = sklearn.model_selection.GridSearchCV(
+            estimator=AdaBoostRegressor(random_state=0),
+            param_grid=param_grid,
+            cv=5)
+        gscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print(param_grid)
+        print(gscv.best_estimator_)
+
+        # estimate on the test set
+        best_gscv = gscv.best_estimator_
+        squared_error = mean_squared_error(
+            y_true=self.y_test,
+            y_pred=best_gscv.predict(self.x_test))
+
+        return squared_error
+
 
     def gaussian_process_regression(self):
+        """
+        for gpr, i train on the training data using different :
+            1)
+            2)
+
+        :return: test accuracy of the gpr best model
+        :return:
+        """
         kernel = DotProduct() + WhiteKernel()
         gpr = Gaussian_process_regressor(k=kernel)
         gpr.train(self.x_train, self.y_train)
         return gpr.get_mse(self.x_test, self.y_test)
 
     def linear_regression(self):
-        lr = Linear_regression()
-        lr.train(self.x_train, self.y_train)
-        return lr.get_mse(self.x_test, self.y_test)
+        """
+        for lr, i train on the training data using same parameters
+
+        :return: test accuracy of the lr best model
+        """
+
+        lr = LinearRegression()
+        lr.fit(self.x_train, self.y_train)
+
+        # estimate on the test set
+        squared_error = mean_squared_error(
+            y_true=self.y_test,
+            y_pred=lr.predict(self.x_test))
+
+        return squared_error
 
     def neural_network_regression(self):
-        nnr = Neural_network_regressor(hls=(15,), s='lbfgs')
-        nnr.train(self.x_train, self.y_train)
-        return nnr.get_mse(self.x_test, self.y_test)
+        """
+        for nnr, i train on the training data using different :
+            1) hidden_layer_sizes
+            2) max_iter
+
+        :return: test accuracy of the nnr best model
+        """
+
+        # RandomSearch Cross Validation
+        # define param_dist argument to give RandomSearchCV
+        reciprocal_distrobution_hls = scipy.stats.reciprocal(a=100, b=1000)
+        reciprocal_distribution_mi = scipy.stats.reciprocal(a=1000, b=10000)
+        np.random.seed(0)
+        reci_hidden_layer_sizes = \
+            reciprocal_distrobution_hls.rvs(size=5).astype(np.int)
+        max_iter = reciprocal_distribution_mi.rvs(size=5).astype(np.int)
+        param_dist = {
+            'hidden_layer_sizes': reci_hidden_layer_sizes,
+            'max_iter': max_iter
+        }
+
+        # fit a 5-fold RandomSearchCV instance
+        rscv = sklearn.model_selection.RandomizedSearchCV(
+            estimator=MLPRegressor(activation='relu', random_state=0),
+            param_distributions=param_dist,
+            verbose=1,
+            cv=5,
+            n_jobs=5,   # 5 concurrent workers
+            n_iter=5,
+            random_state=0)
+        rscv.fit(self.x_train, self.y_train)
+
+        # print the candidates and best parameters
+        print(param_dist)
+        print(rscv.best_estimator_)
+
+        # estimate on the test set
+        best_rscv = rscv.best_estimator_
+        squared_error = mean_squared_error(
+            y_true=self.y_test,
+            y_pred=best_rscv.predict(self.x_test))
+
+        return squared_error
 
 
 if __name__ == '__main__':
     wq = Wine_quality()
-    print('SVR: %.5f' % wq.support_vector_regression())
-    print('DTR: %.5f' % wq.decision_tree_regression())
-    print('RFR: %.5f' % wq.random_forest_regression())
-    print('ABR: %.5f' % wq.ada_boost_regression())
-    print('GPR: %.5f' % wq.gaussian_process_regression())
-    print(' LR: %.5f' % wq.linear_regression())
+    print("mean squared error on the actual test set:")
+    # print('SVR: %.5f' % (wq.support_vector_regression()))
+    # print('DTR: %.5f' % wq.decision_tree_regression())
+    # print('RFR: %.5f' % (wq.random_forest_regression()))
+    # print('ABR: %.5f' % (wq.ada_boost_regression()))
+
+    # print('GPR: %.5f' % wq.gaussian_process_regression())
+
+    # print(' LR: %.5f' % wq.linear_regression())
     print('NNR: %.5f' % wq.neural_network_regression())

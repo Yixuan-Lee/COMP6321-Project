232f18eecd91b71ea09793ea2414e3560209284c
liyixuan <liyixuan4030614@gmail.com>
2019-10-27 22:53:57 -0400

    modify classification dataset1 hyperparameter search

--- models/classification/1_Diabetic_Retinopathy.py
+++ models/classification/1_Diabetic_Retinopathy.py
@@ -2,28 +2,18 @@ import os
 import numpy as np
 import scipy
 import scipy.stats              # For reciprocal distribution
-import sklearn.model_selection  # for RandomizedSearchCV
 from models import settings     # for retrieving root path
 from scipy.io import arff       # for loading .arff file
-from sklearn.neighbors import KNeighborsClassifier
-# from k_nearest_neighbours import K_nearest_neighbours
-from sklearn.svm import SVC
-# from support_vector_classifier import Support_vector_classifier
-from sklearn.tree import DecisionTreeClassifier
-# from decision_tree_classifier import Decision_tree_classifier
-from sklearn.ensemble import RandomForestClassifier
-# from random_forest_classifier import Random_forest_classifier
-from sklearn.ensemble import AdaBoostClassifier
-# from ada_boost_classifier import Ada_boost_classifier
-from sklearn.linear_model import LogisticRegression
-# from logistic_regression import Logistic_regression
-
+from k_nearest_neighbours import K_nearest_neighbours
+from support_vector_classifier import Support_vector_classifier
+from decision_tree_classifier import Decision_tree_classifier
+from random_forest_classifier import Random_forest_classifier
+from ada_boost_classifier import Ada_boost_classifier
+from logistic_regression import Logistic_regression
 from gaussian_naive_bayes import Gaussian_naive_bayes
-from sklearn.neural_network import MLPClassifier
 from neural_network_classifier import Neural_network_classifier
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 
 class Diabetic_retinopathy:
@@ -68,72 +58,56 @@ class Diabetic_retinopathy:
 
         :return: test accuracy of the knn best model
         """
-
-        # Grid Search Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         n_neighbors = range(1, 100, 2)  # [1, 3, 5, ..., 99]
-        weights = ['uniform', 'distance']
-        p = [1, 2]
-        param_grid = {
-            'n_neighbors': n_neighbors,
-            'weights': weights,
-            'p': p
-        }
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=KNeighborsClassifier(),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        knn = K_nearest_neighbours(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            n_neighbors=n_neighbors,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print('candidates: ', param_grid)
-        print(gscv.best_params_)
+        # print all possible parameter values and the best parameters
+        knn.print_parameter_candidates()
+        knn.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        test_accuracy = accuracy_score(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return test_accuracy
+        # return the accuracy score
+        return knn.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def support_vector_classifier(self):
         """
         for svc, i train on the training data using different :
-            2) C
-            3) gamma
+            1) C
+            2) gamma
+            3) kernel
         :return: test accuracy of the svc best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         C = np.logspace(start=-1, stop=3, base=10, num=5, dtype=np.float32)  # [0.1, 1, 10, 100, 1000]
         gamma = np.logspace(start=-1, stop=1, base=10, num=3, dtype=np.float32)  # [0.01, 0.1, 1, 10]
-        param_grid = {
-            'C': C,
-            'gamma': gamma,
-        }
+        kernel = ['linear', 'rbf', 'sigmoid']
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=SVC(kernel='rbf', random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        svc = Support_vector_classifier(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            C=C,
+            kernel=kernel,
+            gamma=gamma,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print('candidates: ', param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values
+        svc.print_parameter_candidates()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        test_accuracy = accuracy_score(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return test_accuracy
+        # return the accuracy score
+        return svc.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def decision_tree_classifier(self):
         """
@@ -144,33 +118,27 @@ class Diabetic_retinopathy:
         :return: test accuracy of the dtc best model
         """
 
-        # GridSearch Cross Validation
         # define param_grid argument to give GridSearchCV
         criterion = ['gini', 'entropy']
         max_depth = range(1, 100, 2)
-        param_grid = {
-            'criterion': criterion,
-            'max_depth': max_depth
-        }
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=DecisionTreeClassifier(random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        dtc = Decision_tree_classifier(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            criterion=criterion,
+            max_depth=max_depth,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print('candidates: ', param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        dtc.print_parameter_candidates()
+        dtc.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        test_accuracy = accuracy_score(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return test_accuracy
+        # return the accuracy score
+        return dtc.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def random_forest_classifier(self):
         """
@@ -180,35 +148,27 @@ class Diabetic_retinopathy:
 
         :return: test accuracy of the dtc best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         criterion = ['gini', 'entropy']
         max_depth = range(1, 20, 2)
-        param_grid = {
-            'criterion': criterion,
-            'max_depth': max_depth
-        }
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=RandomForestClassifier(n_estimators=100, random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        rfc = Random_forest_classifier(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            criterion=criterion,
+            max_depth=max_depth,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print('candidates: ', param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        rfc.print_parameter_candidates()
+        rfc.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        test_accuracy = accuracy_score(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return test_accuracy
-
+        # return the accuracy score
+        return rfc.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def ada_boost_classifier(self):
         """
@@ -218,35 +178,28 @@ class Diabetic_retinopathy:
 
         :return: test accuracy of the dtc best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         n_estimators = range(1, 100, 5)
         learning_rate = np.logspace(start=-2, stop=0, base=10, num=3,
             dtype=np.float32)
-        param_grid = {
-            'n_estimators': n_estimators,
-            'learning_rate': learning_rate
-        }
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=AdaBoostClassifier(algorithm='SAMME', random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        abc = Ada_boost_classifier(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            n_estimators=n_estimators,
+            learning_rate=learning_rate,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print('candidates: ', param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        abc.print_parameter_candidates()
+        abc.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        test_accuracy = accuracy_score(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return test_accuracy
+        # return the accuracy score
+        return abc.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def logistic_regression(self):
         """
@@ -255,45 +208,53 @@ class Diabetic_retinopathy:
 
         :return: test accuracy of the dtc best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         C = np.logspace(start=-4, stop=4, base=10, num=9, dtype=np.float32)
-        param_grid = {
-            'C': C
-        }
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=LogisticRegression(max_iter=10000, solver='lbfgs',
-                random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        lr = Logistic_regression(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            C=C,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print('candidates: ', param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        lr.print_parameter_candidates()
+        lr.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        test_accuracy = accuracy_score(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return test_accuracy
+        # return the accuracy score
+        return lr.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def gaussian_naive_bayes(self):
         """
         for gnb, i train on the training data using different :
-            1)
-            2)
+            1) var_smoothing
 
         :return: test accuracy of the gnb best model
         """
-        gnb = Gaussian_naive_bayes()
-        gnb.train(self.x_train, self.y_train)
-        return gnb.get_accuracy(self.x_test, self.y_test)
+        # define arguments given to GridSearchCV
+        var_smoothing = np.logspace(start=-9, stop=-6, base=10, num=4,
+            dtype=np.float32)
+
+        # get the best validated model
+        gnb = Gaussian_naive_bayes(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            var_smoothing=var_smoothing,
+            grid_search=True)
+
+        # print all possible parameter values and the best parameters
+        gnb.print_parameter_candidates()
+        gnb.print_best_estimator()
+
+        # return the accuracy score
+        return gnb.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def neural_network_classifier(self):
         """
@@ -303,54 +264,41 @@ class Diabetic_retinopathy:
 
         :return: test accuracy of the nnr best model
         """
-        # RandomSearch Cross Validation
-        # define param_dist argument to give RandomSearchCV
+        # define arguments given to RandomSearchCV
         reciprocal_distrobution_hls = scipy.stats.reciprocal(a=100, b=1000)
         reciprocal_distribution_mi = scipy.stats.reciprocal(a=1000, b=10000)
         np.random.seed(0)
-        reci_hidden_layer_sizes = \
+        hidden_layer_sizes = \
             reciprocal_distrobution_hls.rvs(size=5).astype(np.int)
         max_iter = reciprocal_distribution_mi.rvs(size=5).astype(np.int)
-        param_dist = {
-            'hidden_layer_sizes': reci_hidden_layer_sizes,
-            'max_iter': max_iter
-        }
 
-        # fit a 5-fold RandomSearchCV instance
-        rscv = sklearn.model_selection.RandomizedSearchCV(
-            estimator=MLPClassifier(activation='relu', solver='lbfgs',
-                alpha=1e-5, random_state=0),
-            param_distributions=param_dist,
-            verbose=1,
-            cv=5,
-            n_jobs=5,  # 5 concurrent workers
-            n_iter=5,
-            random_state=0)
-        rscv.fit(self.x_train, self.y_train)
+        # get the best random validated model
+        nnc = Neural_network_classifier(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            hidden_layer_sizes=hidden_layer_sizes,
+            max_iter=max_iter,
+            random_search=True)
 
-        # print the candidates and best parameters
-        print('candidates: ', param_dist)
-        print(rscv.best_estimator_)
+        # print all possible parameter values and best parameters
+        nnc.print_parameter_candidates()
+        nnc.print_best_estimator()
 
-        # estimate on the test set
-        best_rscv = rscv.best_estimator_
-        test_accuracy = accuracy_score(
-            y_true=self.y_test,
-            y_pred=best_rscv.predict(self.x_test))
-
-        return test_accuracy
+        # return the accuracy score
+        return nnc.accuracy_score(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
 
 if __name__ == '__main__':
     dr = Diabetic_retinopathy()
     print("accuracy on the actual test set:")
-    # print('KNN: %.2f %%' % (dr.k_nearest_neighbours() * 100))
-    # print('SVC: %.2f %%' % (dr.support_vector_classifier() * 100))
-    # print('DTC: %.2f %%' % (dr.decision_tree_classifier() * 100))
-    # print('RFC: %.2f %%' % (dr.random_forest_classifier() * 100))
-    # print('ABC: %.2f %%' % (dr.ada_boost_classifier() * 100))
-    # print(' LR: %.2f %%' % (dr.logistic_regression() * 100))
-
-    # print('GNB: %.2f %%' % (dr.gaussian_naive_bayes() * 100))
-
+    print('KNN: %.2f %%' % (dr.k_nearest_neighbours() * 100))
+    print('SVC: %.2f %%' % (dr.support_vector_classifier() * 100))
+    print('DTC: %.2f %%' % (dr.decision_tree_classifier() * 100))
+    print('RFC: %.2f %%' % (dr.random_forest_classifier() * 100))
+    print('ABC: %.2f %%' % (dr.ada_boost_classifier() * 100))
+    print(' LR: %.2f %%' % (dr.logistic_regression() * 100))
+    print('GNB: %.2f %%' % (dr.gaussian_naive_bayes() * 100))
     print('NNC: %.2f %%' % (dr.neural_network_classifier() * 100))

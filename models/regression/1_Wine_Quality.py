9c9cb2c8691b244b67e3dc4c0107c865e8f7e883
liyixuan <liyixuan4030614@gmail.com>
2019-10-28 20:26:05 -0400

    update code for regression dataset_1

--- models/regression/1_Wine_Quality.py
+++ models/regression/1_Wine_Quality.py
@@ -2,26 +2,17 @@ import os
 import numpy as np
 import scipy
 import scipy.stats              # For reciprocal distribution
-import sklearn.model_selection  # for RandomizedSearchCV
 from models import settings     # for retrieving root path
-from sklearn.svm import SVR
 from support_vector_regressor import Support_vector_regressor
-from sklearn.tree import DecisionTreeRegressor
 from decision_tree_regressor import Decision_tree_regressor
-from sklearn.ensemble import RandomForestRegressor
 from random_forest_regressor import Random_forest_regressor
-from sklearn.ensemble import AdaBoostRegressor
 from ada_boost_regressor import Ada_boost_regressor
-
 from gaussian_process_regressor import Gaussian_process_regressor
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
-from sklearn.linear_model import LinearRegression
 from linear_regression import Linear_regression
-from sklearn.neural_network import MLPRegressor
 from neural_network_regressor import Neural_network_regressor
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 
 class Wine_quality:
@@ -46,6 +37,12 @@ class Wine_quality:
         self.data = file_data[:, :-1]  # (6497, 11)
         self.targets = file_data[:, -1]  # (6497, )
 
+
+        # subsampling data (for speeding up)
+        # self.data = self.data[:600]
+        # self.targets = self.targets[:600]
+
+
         # split into train and test sets
         self.x_train, self.x_test, self.y_train, self.y_test = \
             train_test_split(self.data, self.targets, test_size=0.33,
@@ -63,76 +60,64 @@ class Wine_quality:
         for svr, i train on the training data using different :
             1) C
             2) gamma
-
+            3) kernel
         :return: test accuracy of the svr best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         C = np.logspace(start=-1, stop=3, base=10, num=5,
             dtype=np.float32)  # [0.1, 1, 10, 100, 1000]
         gamma = np.logspace(start=-1, stop=1, base=10, num=3,
             dtype=np.float32)  # [0.01, 0.1, 1, 10]
-        param_grid = {
-            'C': C,
-            'gamma': gamma,
-        }
+        kernel = ['rbf', 'linear', 'sigmoid']
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=SVR(kernel='rbf'),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        svr = Support_vector_regressor(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            C=C,
+            kernel=kernel,
+            gamma=gamma,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print(param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        svr.print_parameter_candidates()
+        svr.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        squared_error = mean_squared_error(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return squared_error
+        # return the mean squared error
+        return svr.mean_sqaured_error(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def decision_tree_regression(self):
         """
         for dtr, i train on the training data using different :
-            1) criterion
-            2) max_depth
+            1) max_depth
+            2) min_samples_leaf
 
         :return: test accuracy of the dtr best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
-        criterion = ['mse', 'mae']
+        # define arguments given to GridSearchCV
         max_depth = range(1, 20, 2)
-        param_grid = {
-            'criterion': criterion,
-            'max_depth': max_depth
-        }
+        min_samples_leaf = (1, 20, 2)
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=DecisionTreeRegressor(random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        dtr = Decision_tree_regressor(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            max_depth=max_depth,
+            min_samples_leaf=min_samples_leaf,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print(param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        dtr.print_parameter_candidates()
+        dtr.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        squared_error = mean_squared_error(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return squared_error
+        # return the mean squared error
+        return dtr.mean_squared_error(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def random_forest_regression(self):
         """
@@ -142,36 +127,27 @@ class Wine_quality:
 
         :return: test accuracy of the rfr best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         n_estimators = range(1, 200, 50)
         max_depth = range(1, 20, 2)
-        param_grid = {
-            'n_estimators': n_estimators,
-            'max_depth': max_depth
-        }
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=RandomForestRegressor(random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        rfr = Random_forest_regressor(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print(param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        rfr.print_parameter_candidates()
+        rfr.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        squared_error = mean_squared_error(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return squared_error
-
-
+        # return the mean squared error
+        return rfr.mean_squared_error(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def ada_boost_regression(self):
         """
@@ -181,50 +157,60 @@ class Wine_quality:
 
         :return: test accuracy of the abr best model
         """
-
-        # GridSearch Cross Validation
-        # define param_grid argument to give GridSearchCV
+        # define arguments given to GridSearchCV
         n_estimators = range(1, 100, 5)
         learning_rate = np.logspace(start=-2, stop=0, base=10, num=3,
             dtype=np.float32)   # [0.01, 0.1, 1]
-        param_grid = {
-            'n_estimators': n_estimators,
-            'learning_rate': learning_rate
-        }
 
-        # fit a 5-fold GridSearchCV instance
-        gscv = sklearn.model_selection.GridSearchCV(
-            estimator=AdaBoostRegressor(random_state=0),
-            param_grid=param_grid,
-            cv=5)
-        gscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        abr = Ada_boost_regressor(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=5,
+            n_estimators=n_estimators,
+            learning_rate=learning_rate,
+            grid_search=True)
 
-        # print the candidates and best parameters
-        print(param_grid)
-        print(gscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        abr.print_parameter_candidates()
+        abr.print_best_estimator()
 
-        # estimate on the test set
-        best_gscv = gscv.best_estimator_
-        squared_error = mean_squared_error(
-            y_true=self.y_test,
-            y_pred=best_gscv.predict(self.x_test))
-
-        return squared_error
-
+        # return the mean squared error
+        return abr.mean_squared_error(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def gaussian_process_regression(self):
         """
         for gpr, i train on the training data using different :
-            1)
-            2)
+            1) alpha
+            2) kernel (will raise an exception)
 
         :return: test accuracy of the gpr best model
         :return:
         """
-        kernel = DotProduct() + WhiteKernel()
-        gpr = Gaussian_process_regressor(k=kernel)
-        gpr.train(self.x_train, self.y_train)
-        return gpr.get_mse(self.x_test, self.y_test)
+        # define arguments given to GridSearchCV
+        # kernel = [DotProduct(), WhiteKernel(), DotProduct() + WhiteKernel()]
+        alpha = np.logspace(start=-10, stop=-7, base=10, num=4,
+            dtype=np.float32)
+
+        # get the best validated model
+        gpr = Gaussian_process_regressor(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            # kernel=kernel,
+            alpha=alpha,
+            grid_search=True)
+
+        # print all possible parameter values and the best parameters
+        gpr.print_parameter_candidates()
+        gpr.print_best_estimator()
+
+        # return the mean squared error
+        return gpr.mean_squared_error(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def linear_regression(self):
         """
@@ -232,72 +218,65 @@ class Wine_quality:
 
         :return: test accuracy of the lr best model
         """
-
-        lr = LinearRegression()
-        lr.fit(self.x_train, self.y_train)
+        # get the best validated model
+        lr = Linear_regression(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3)
 
         # estimate on the test set
-        squared_error = mean_squared_error(
-            y_true=self.y_test,
-            y_pred=lr.predict(self.x_test))
-
-        return squared_error
+        return lr.mean_squared_error(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
     def neural_network_regression(self):
         """
         for nnr, i train on the training data using different :
             1) hidden_layer_sizes
-            2) max_iter
+            2) activation
+            3) max_iter
 
         :return: test accuracy of the nnr best model
         """
-
-        # RandomSearch Cross Validation
-        # define param_dist argument to give RandomSearchCV
+        # define arguments given to RandomSearchCV
         reciprocal_distrobution_hls = scipy.stats.reciprocal(a=100, b=1000)
         reciprocal_distribution_mi = scipy.stats.reciprocal(a=1000, b=10000)
         np.random.seed(0)
-        reci_hidden_layer_sizes = \
+        hidden_layer_sizes = \
             reciprocal_distrobution_hls.rvs(size=5).astype(np.int)
+        activation = ['logistic', 'tanh', 'relu']
         max_iter = reciprocal_distribution_mi.rvs(size=5).astype(np.int)
-        param_dist = {
-            'hidden_layer_sizes': reci_hidden_layer_sizes,
-            'max_iter': max_iter
-        }
 
-        # fit a 5-fold RandomSearchCV instance
-        rscv = sklearn.model_selection.RandomizedSearchCV(
-            estimator=MLPRegressor(activation='relu', random_state=0),
-            param_distributions=param_dist,
-            verbose=1,
-            cv=5,
-            n_jobs=5,   # 5 concurrent workers
-            n_iter=5,
-            random_state=0)
-        rscv.fit(self.x_train, self.y_train)
+        # get the best validated model
+        nnr = Neural_network_regressor(
+            x_train=self.x_train,
+            y_train=self.y_train,
+            cv=3,
+            hidden_layer_sizes=hidden_layer_sizes,
+            activation=activation,
+            max_iter=max_iter,
+            random_search=True)
 
-        # print the candidates and best parameters
-        print(param_dist)
-        print(rscv.best_estimator_)
+        # print all possible parameter values and the best parameters
+        nnr.print_parameter_candidates()
+        nnr.print_best_estimator()
 
-        # estimate on the test set
-        best_rscv = rscv.best_estimator_
-        squared_error = mean_squared_error(
-            y_true=self.y_test,
-            y_pred=best_rscv.predict(self.x_test))
-
-        return squared_error
+        # # return the mean squared error
+        return nnr.mean_squared_error(
+            x_test=self.x_test,
+            y_test=self.y_test)
 
 
 if __name__ == '__main__':
     wq = Wine_quality()
     print("mean squared error on the actual test set:")
-    # print('SVR: %.5f' % (wq.support_vector_regression()))
-    # print('DTR: %.5f' % wq.decision_tree_regression())
-    # print('RFR: %.5f' % (wq.random_forest_regression()))
-    # print('ABR: %.5f' % (wq.ada_boost_regression()))
+    print('SVR: %.5f' % (wq.support_vector_regression()))
+    print('DTR: %.5f' % wq.decision_tree_regression())
+    print('RFR: %.5f' % (wq.random_forest_regression()))
+    print('ABR: %.5f' % (wq.ada_boost_regression()))
 
-    # print('GPR: %.5f' % wq.gaussian_process_regression())
+    # iterating over kernels will raise a bug
+    print('GPR: %.5f' % wq.gaussian_process_regression())
 
-    # print(' LR: %.5f' % wq.linear_regression())
+    print(' LR: %.5f' % wq.linear_regression())
     print('NNR: %.5f' % wq.neural_network_regression())

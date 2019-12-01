6e6eecd70d9a604548040268d8eaef42b0c01099
Rui Li <tancredo29@gmail.com>
2019-12-01 10:44:06 -0500

    Update preprocessing file of Merck data.

--- models/regression/10_prep_data.py
+++ models/regression/10_prep_data.py
@@ -1,7 +1,5 @@
 import os
 import numpy as np
-import sklearn
-from sklearn.model_selection import train_test_split
 
 from models import settings
 
@@ -26,33 +24,9 @@ y1 = np.loadtxt(MERCK_FILE1, delimiter=',', usecols=[1], skiprows=1)
 X2 = np.loadtxt(MERCK_FILE2, delimiter=',', usecols=range(2, len(cols2)), skiprows=1, dtype=np.uint8)
 y2 = np.loadtxt(MERCK_FILE2, delimiter=',', usecols=[1], skiprows=1)
 
-# separate into train and test sets
-x_train1, x_test1, y_train1, y_test1 = \
-    train_test_split(X1, y1, test_size=0.33,
-                     random_state=0)
-# print(self.x_train[:10],self.y_train[:10])
-
-# normalize the training set
-scaler = sklearn.preprocessing.StandardScaler().fit(x_train1)
-x_train1 = scaler.transform(x_train1)
-# normalize the test set with the train-set mean and std
-x_test1 = scaler.transform(x_test1)
-
-# separate into train and test sets
-x_train2, x_test2, y_train2, y_test2 = \
-    train_test_split(X2, y2, test_size=0.33,
-                     random_state=0)
-# print(self.x_train[:10],self.y_train[:10])
-
-# normalize the training set
-scaler = sklearn.preprocessing.StandardScaler().fit(x_train2)
-x_train2 = scaler.transform(x_train2)
-# normalize the test set with the train-set mean and std
-x_test2 = scaler.transform(x_test2)
-
 outfile1 = os.path.join(settings.ROOT_DIR, filepath, "Merck_Data1.npz")
 outfile2 = os.path.join(settings.ROOT_DIR, filepath, "Merck_Data2.npz")
 
-np.savez_compressed(outfile1, x_train1=x_train1, x_test1=x_test1, y_train1=y_train1, y_test1=y_test1)
+np.savez_compressed(outfile1, X=X1, y=y1)
 
-np.savez_compressed(outfile2, x_train2=x_train2, x_test2=x_test2, y_train2=y_train2, y_test2=y_test2)
+np.savez_compressed(outfile2, X=X2, y=y2)
